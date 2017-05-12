927f3e089f9d4aabca849c8519d6165c4d35b65e
paltapas3 <paltapas3@gmail.com>
2017-05-13 01:09:36 +0530

    Updating latest changes

---
+++ CallBackFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace CallBackFunction
+ {
+    public delegate void TaskCompletedCallBack(string taskResult);
+    public class CallBackFunction
+    {
+
+        public void StartNewTask(TaskCompletedCallBack taskCompletedCallBack)
+        {
+            Console.WriteLine("I have started new Task.");
+            if (taskCompletedCallBack != null)
+                taskCompletedCallBack("I have completed Task.");
+        }
+        static void Main(string[] args)
+        {
+
+            CallBackTest callBackTest = new CallBackTest();
+            callBackTest.Test();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallBackTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallBackFunction
+{
+    public class CallBackTest
+    {
+        public void Test()
+        {
+            TaskCompletedCallBack callback = TestCallBack;
+            CallBackFunction testCallBack = new CallBackFunction();
+            testCallBack.StartNewTask(callback);
+        }
+        public void TestCallBack(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
